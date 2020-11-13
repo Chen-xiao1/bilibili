7fc69084331d0a8766d9d39751b9e25b769f1cdf
chenxiao <you@example.com>
2020-11-13 22:03:15 +0800

    bili

---
+++ bilibili/request.js
@@ -0,0 +1,9 @@
+axios.defaults.baseURL='https://developer.duyiedu.com/vue/bz/';
+
+axios.interceptors.response.use(response=>{
+    const { status }=response;
+    if(status===200){
+        return response.data;
+    }
+    return response;
+})
